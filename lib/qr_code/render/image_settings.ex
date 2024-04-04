ccad69ec342fe7b78fe6c42345875572857d532f
Ash Malmsten <32045016+skipday@users.noreply.github.com>
2024-04-04 16:49:26 +0200

    bug

--- lib/qr_code/render/image_settings.ex
+++ lib/qr_code/render/image_settings.ex
@@ -6,12 +6,12 @@ defmodule QRCode.Render.ImageSettings do
   @type t :: %__MODULE__{
           path: ExMaybe.t(binary()),
           data: ExMaybe.t(binary()),
-          size: ExMaybe.t(pos_integer()),
+          size: pos_integer(),
           client_type: ExMaybe.t(binary())
         }
 
   defstruct path: nil,
             data: nil,
-            size: 0,
+            size: 50,
             client_type: nil
 end
